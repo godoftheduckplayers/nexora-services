c92419a67261904671935dfc2a0f7a120a0e2b75
Leandro Marques <lmarqueo@emeal.nttdata.com>
2026-05-13 22:55:58 -0300

    Init gateway project

---
+++ apps/nexora-scooby-doo/src/main/java/com/nexora/scooby_doo/NexoraScoobyDooApplication.java
@@ -0,0 +1,12 @@
+package com.nexora.scooby_doo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraScoobyDooApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NexoraScoobyDooApplication.class, args);
+  }
+}
---
+++ apps/nexora-scooby-doo/src/test/java/com/nexora/scooby_doo/NexoraScoobyDooApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nexora.scooby_doo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraScoobyDooApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
