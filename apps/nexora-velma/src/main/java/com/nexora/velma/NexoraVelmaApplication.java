16bbecfb8147567d7f0489f9efc6c2e333bd0cb0
Leandro Marques <lmarqueo@emeal.nttdata.com>
2026-05-15 23:48:51 -0300

    chore(velma): initialize project structure

---
+++ apps/nexora-velma/src/main/java/com/nexora/velma/NexoraVelmaApplication.java
@@ -0,0 +1,12 @@
+package com.nexora.velma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraVelmaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NexoraVelmaApplication.class, args);
+  }
+}
---
+++ apps/nexora-velma/src/test/java/com/nexora/velma/NexoraVelmaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nexora.velma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraVelmaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
